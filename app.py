f17b57372ffcd44495e60c6127cf0bd87bf6b1f1
zelys <zelys.dev@gmail.com>
2025-08-14 13:56:21 -0400

    update files

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-from flask import Flask, render_template, request, send_file
-from utils import tabla_amort_en_uf, exportar_excel, exportar_pdf, formatear_dataframe
+from flask import Flask, render_template, request, send_file, redirect, url_for
+from utils import tabla_amort_en_uf, tabla_amortizacion_en_clp, exportar_excel, exportar_pdf, formatear_dataframe, obtener_valor_uf
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     tabla = None
+    valor_uf = obtener_valor_uf()
+    fecha_actual = datetime.now().strftime('%d/%m/%Y')
+    
     if request.method == 'POST':
         valor = float(request.form['valor'])
         porcentaje = float(request.form['porcentaje'])
         tasa = float(request.form['tasa'])
         plazo = int(request.form['plazo'])
-        tabla = tabla_amort_en_uf(valor, porcentaje, tasa, plazo)
-    return render_template('index.html', tabla=tabla)
+        tipo_moneda = request.form.get('tipo_moneda', 'uf')
+        
+        if tipo_moneda == 'clp':
+            tabla = tabla_amortizacion_en_clp(valor, porcentaje, tasa, plazo)
+        else:
+            tabla = tabla_amort_en_uf(valor, porcentaje, tasa, plazo)
+            
+    return render_template('index.html', tabla=tabla, valor_uf=valor_uf, fecha_actual=fecha_actual)
+
+# Ruta para nuevo cálculo - redirecciona al inicio
+@app.route('/nuevo-calculo', methods=['GET'])
+def nuevo_calculo():
+    return redirect(url_for('index'))
 
 # Route para descargar la tabla en diferentes formatos
 @app.route('/descargar/<formato>', methods=['POST'])
@@ -20,14 +35,22 @@ def descargar(formato):
     porcentaje = float(request.form['porcentaje'])
     tasa = float(request.form['tasa'])
     plazo = int(request.form['plazo'])
-    tabla = tabla_amort_en_uf(valor, porcentaje, tasa, plazo)
+    tipo_moneda = request.form.get('tipo_moneda', 'uf')
+    fecha_actual = datetime.now().strftime('%d/%m/%Y')
+    
+    if tipo_moneda == 'clp':
+        tabla = tabla_amortizacion_en_clp(valor, porcentaje, tasa, plazo)
+        sufijo_moneda = '_clp'
+    else:
+        tabla = tabla_amort_en_uf(valor, porcentaje, tasa, plazo)
+        sufijo_moneda = '_uf'
     
     if formato == 'excel':
         output = exportar_excel(formatear_dataframe(tabla))
-        return send_file(output, as_attachment=True, download_name='amortizacion.xlsx')
+        return send_file(output, as_attachment=True, download_name=f'amortizacion{sufijo_moneda}.xlsx')
     elif formato == 'pdf':
         output = exportar_pdf(formatear_dataframe(tabla))
-        return send_file(output, as_attachment=True, download_name='amortizacion.pdf')
+        return send_file(output, as_attachment=True, download_name=f'amortizacion{sufijo_moneda}.pdf')
     else:
         return 'Formato no soportado', 400
 
